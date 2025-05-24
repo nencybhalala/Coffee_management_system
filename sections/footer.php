309adbb644016be5a2d226867adaeee2b8a9dbd3
nencybhalala <nencybhalala02@gmail.com>
2025-05-24 18:21:45 +0530

    Add files via upload

---
+++ sections/footer.php
@@ -0,0 +1,43 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>locations</h3>
+            <a href="#">India</a>
+            <a href="#">Italy</a>
+            <a href="#">Russia</a>
+            <a href="#">USA</a>
+            <a href="#">France</a>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="./index.php">Home</a>
+            <!-- <a href="offer.php">Offers</a> -->
+            <a href="./about.php">About</a>
+            <a href="menu.php">Menu</a>
+            <a href="extras.php">Toppings</a>
+            <a href="review.php">Reviews</a>
+            <a href="./contactus.php">Contact Us</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#">+91 1234567809</a>
+            <a href="#">+91 1234567890</a>
+            <a href="#">support@nescafe.com</a>
+        </div>
+
+        <div class="box">
+            <h3>follow me</h3>
+            <a href="#">facebook</a>
+            <a href="#">twitter</a>
+            <a href="#">instagram</a>
+        </div>
+
+    </div>
+
+    <div class="credit"> &copy; 2025 Nescafe. All rights reserved. </div>
+
+</section>
